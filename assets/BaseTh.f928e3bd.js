8b6b24fb0d8912b429a6380cd30168d94b43b7e4
yeonghoonPark <95198410+yeonghoonPark@users.noreply.github.com>
2023-01-25 19:46:37 +0900

    Add files via upload

---
+++ assets/BaseTh.f928e3bd.js
@@ -0,0 +1,1 @@
+import{o as t,c as a,l as o,t as r,z as n,v as c}from"./index.3f6e2155.js";const p=["scope"],i={__name:"BaseTh",props:{scopeStyle:{type:String,required:!1},message:{type:[String,Number],required:!1}},setup(e){return(s,l)=>(t(),a("th",{scope:e.scopeStyle},[o(r(e.message)+" ",1),s.$slots.span?n(s.$slots,"span",{key:0}):c("",!0)],8,p))}};export{i as _};
